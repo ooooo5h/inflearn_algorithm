8547b084f59fcfdf309d3d886f667c880d9c99e7
전은형 <ooooo5h91@gmail.com>
2022-02-21 13:14:38 +0900

    재귀함수를 이용한 이진수 출력 -> 에러
    - 1011이 나와야 하는데 1101로 나옴

--- 완전탐색-DFS 기초/1.재귀함수이용한이진수출력.py
+++ 완전탐색-DFS 기초/1.재귀함수이용한이진수출력.py
@@ -2,15 +2,14 @@
 # 10진수 n이 입력하면 2진수로 변환하여 출력하는 프로그램을 작성해라
 
 def change_to_binary(n):
+    # 재귀함수 사용 시 종료조건 필수!!
+    if n == 0:
+        return
     
-    answer = []
-    while n >= 1:    
-        answer.append(n % 2)
-        n = n // 2
-    
-    answer.reverse()
-    
-    for i in answer:
-        print(i, end='')
+    else :
+        # n을 2 로 나눈 나머지가 이진수에 해당하는 숫자
+        print(n%2, end='')
+        # n을 2로 나눈 몫은 또 나눠줘야하니까 재귀함수호출 시 파라미터로 전달
+        change_to_binary(n//2)
     
 change_to_binary(11)

a3ccba46eb1f5831952e4ebe8c59ece3e79eda6f
eunhyung <ooooo5h91@gmail.com>
2022-04-13 15:43:09 +0900

    str과 int를 사용한 파이썬스러운 풀이(자릿수의 합)

--- 1.코드구현력기르기/6.자릿수의합.py
+++ 1.코드구현력기르기/6.자릿수의합.py
@@ -3,14 +3,12 @@ nlist = list(map(int, input().split()))
 
 def digit_sum(x) :
     sum = 0 
-    for _ in range(len(str(x))) :
-        a = x % 10
-        x = x // 10
-        sum += a
+    for i in str(x):   # 파이썬스럽게 => str로 int처리하면 한 자리씩 사용 가능.
+        sum += int(i)
         
     return sum
 
-# print(digit_sum(7))
+# print(digit_sum(7))  
 # print(digit_sum(127))
 # print(digit_sum(2254))
 
